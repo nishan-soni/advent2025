2c9aa7b6d485bffcfbf23e33bef542eeb968046a
nishan-soni <nishan.soni2@gmail.com>
2025-12-10 01:33:32 -0700

    day 1

--- day1/solve.go
+++ day1/solve.go
@@ -1,21 +1,36 @@
-package day1 
+package day1
 
 import (
 	"fmt"
+	"strconv"
+
 	"github.com/nishan-soni/advent2025/utils"
 )
 
-
 func Solve() {
-	lines, err := utils.GetFileLines("input.txt")
+	lines, err := utils.GetFileLines("inputs/day1.txt")
 
 	if err != nil {
 		panic(err)
 	}
-	
+
+	curr := 50
+	ans := 0
+
 	for line := range lines {
-		fmt.Println(line)
+		count, _ := strconv.Atoi(line[1:])
+		direction := line[:1]
+
+		if direction == "L" {
+			curr = (curr - count) % 100
+		} else {
+			curr = (curr + count) % 100
+		}
+
+		if curr == 0 {
+			ans++
+		}
+
 	}
-
+	fmt.Println(ans)
 }
-
